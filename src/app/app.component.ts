cfc46a342c57be7ad3dca3a813009be6fbd4a84a
syed momin <syedmomin168@gmail.com>
2025-01-17 22:55:40 +0500

    refactor: clean up unused imports and styles; enhance app layout with toolbar and card components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 @Component({
   selector: 'app-root',
   imports: [
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
     RouterOutlet,
     RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+
 }
